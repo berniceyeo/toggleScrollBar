8f16b73f97f37cf004be75ae0460541256a5727e
berniceyeo <berniceyeosuping@gmail.com>
2023-07-04 13:38:28 +0800

    feature: error boundary

--- src/pages/ApiDetails.tsx
+++ src/pages/ApiDetails.tsx
@@ -9,6 +9,7 @@ import {
   ButtonContainer,
 } from "./ApiDetailsStyle";
 import { Button } from "../components/Button/Button";
+import ErrorBoundary from "../components/Error/ErrorBoundary";
 
 function ApiDetails(props: any) {
   const location = useLocation();
@@ -17,74 +18,76 @@ function ApiDetails(props: any) {
   const logo = state["info"]["x-logo"]["url"];
 
   return (
-    <Container>
-      <Header className="header">
-        <div>{logo ? <Image src={`${logo}`} alt="logo" /> : ""}</div>
-        <div>{state.info.title}</div>
-      </Header>
-      <SubItems $size={"22px"} $marginBottom={"10px"}>
-        Description
-      </SubItems>
+    <ErrorBoundary>
+      <Container>
+        <Header className="header">
+          <div>{logo ? <Image src={`${logo}`} alt="logo" /> : ""}</div>
+          <div>{state.info.title}</div>
+        </Header>
+        <SubItems $size={"22px"} $marginBottom={"10px"}>
+          Description
+        </SubItems>
 
-      <SubItems $size={"16px"} $marginBottom={"50px"}>
-        {state.info.description ?? "Nil"}
-      </SubItems>
-      <SubItems $size={"22px"} $marginBottom={"10px"}>
-        Swagger
-      </SubItems>
-      <SubItems $size={"16px"} $marginBottom={"50px"}>
-        {state.swaggerUrl ?? "Nil"}
-      </SubItems>
-      <SubItems $size={"22px"} $marginBottom={"10px"}>
-        Contact
-      </SubItems>
-      <div>
-        <Table className="table">
-          {state.info.contact ? (
-            <>
-              {state.info.contact.email && (
-                <>
-                  <TableSection $span={1}>Email</TableSection>
-                  <TableSection $span={7}>
-                    {state.info.contact?.email}
-                  </TableSection>
-                </>
-              )}
-              {state.info.contact.name && (
-                <>
-                  <TableSection $span={1}>Name</TableSection>
-                  <TableSection $span={7}>
-                    {state.info.contact?.name ?? " "}
-                  </TableSection>
-                </>
-              )}
+        <SubItems $size={"16px"} $marginBottom={"50px"}>
+          {state.info.description ?? "Nil"}
+        </SubItems>
+        <SubItems $size={"22px"} $marginBottom={"10px"}>
+          Swagger
+        </SubItems>
+        <SubItems $size={"16px"} $marginBottom={"50px"}>
+          {state.swaggerUrl ?? "Nil"}
+        </SubItems>
+        <SubItems $size={"22px"} $marginBottom={"10px"}>
+          Contact
+        </SubItems>
+        <div>
+          <Table className="table">
+            {state.info.contact ? (
+              <>
+                {state.info.contact.email && (
+                  <>
+                    <TableSection $span={1}>Email</TableSection>
+                    <TableSection $span={7}>
+                      {state.info.contact?.email}
+                    </TableSection>
+                  </>
+                )}
+                {state.info.contact.name && (
+                  <>
+                    <TableSection $span={1}>Name</TableSection>
+                    <TableSection $span={7}>
+                      {state.info.contact?.name ?? " "}
+                    </TableSection>
+                  </>
+                )}
 
-              {state.info.contact.url && (
-                <>
-                  <TableSection $span={1}>Url</TableSection>
-                  <TableSection $span={7}>
-                    {state.info.contact?.url}
-                  </TableSection>
-                </>
-              )}
-            </>
-          ) : (
-            "Nil"
-          )}
-        </Table>
-      </div>
-      <ButtonContainer>
-        <Button
-          $disabled={false}
-          handleClick={() => {
-            navigate("/");
-          }}
-          $color={"rgba(4,157,210)"}
-        >
-          Explore more APIs
-        </Button>
-      </ButtonContainer>
-    </Container>
+                {state.info.contact.url && (
+                  <>
+                    <TableSection $span={1}>Url</TableSection>
+                    <TableSection $span={7}>
+                      {state.info.contact?.url}
+                    </TableSection>
+                  </>
+                )}
+              </>
+            ) : (
+              "Nil"
+            )}
+          </Table>
+        </div>
+        <ButtonContainer>
+          <Button
+            $disabled={false}
+            handleClick={() => {
+              navigate("/");
+            }}
+            $color={"rgba(4,157,210)"}
+          >
+            Explore more APIs
+          </Button>
+        </ButtonContainer>
+      </Container>
+    </ErrorBoundary>
   );
 }
 
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Button } from "../components/Button/Button";
 import { Sidebar } from "../components/Sidebar/Sidebar";
 import { getApis } from "../services/api";
+import ErrorBoundary from "../components/Error/ErrorBoundary";
 
 function MainPage() {
   const [expanded, setExpanded] = useState(false);
@@ -18,26 +19,28 @@ function MainPage() {
   };
 
   return (
-    <div className="App">
-      <div className="main-content">
-        <Sidebar expanded={expanded} items={providers} />
-        <div>
-          <div
-            onClick={toggleNav}
-            className={
-              expanded ? "overlay overlay-expanded" : "overlay-content"
-            }
-          ></div>
-          <Button
-            $disabled={expanded}
-            $color={"rgba(4,157,210)"}
-            handleClick={toggleNav}
-          >
-            Explore web APIs
-          </Button>
+    <ErrorBoundary>
+      <div className="App">
+        <div className="main-content">
+          <Sidebar expanded={expanded} items={providers} />
+          <div>
+            <div
+              onClick={toggleNav}
+              className={
+                expanded ? "overlay overlay-expanded" : "overlay-content"
+              }
+            ></div>
+            <Button
+              $disabled={expanded}
+              $color={"rgba(4,157,210)"}
+              handleClick={toggleNav}
+            >
+              Explore web APIs
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+    </ErrorBoundary>
   );
 }
 
